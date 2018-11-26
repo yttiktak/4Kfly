c6321359037a8286517dae427d5573e0a0368920
Roberta <repeatingshadow@yahoo.com>
2018-11-26 13:54:59 -0500

    gyro. But need jdk for android compile

--- Assets/Scripts/MotionControl.cs
+++ Assets/Scripts/MotionControl.cs
@@ -1,19 +1,44 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.Networking;
 
 public class MotionControl : NetworkBehaviour {
+    Gyroscope gyr;
+    void Start() {
+       // if (Input.gyro.enabled) 
+        gyr = Input.gyro;
+        Vector3 grav = gyr.gravity;
+        if (grav.magnitude == 0)
+        {
+            gyr.enabled = false;
+        }
+        else
+        {
+            gyr.enabled = true;
+        }
+    }
+
     void Update() {
         if (!isLocalPlayer) { return; }
 
-        var x = Input.GetAxis("Horizontal") * Time.deltaTime * 150.0f;
-        var z = Input.GetAxis("Vertical") * Time.deltaTime * 5.0f;
-        var y = Input.GetAxis("Pitch") * Time.deltaTime * 150.0f;
-        var r = Input.GetAxis("Roll") * Time.deltaTime * 150.0f;
+        if (gyr.enabled)
+        {
+            Quaternion gq = gyr.attitude;
+            transform.rotation = gq;
+        }
+        else
+        {
 
-        transform.Rotate(y, x, r);
-        transform.Translate(0, 0, -z);
+            var x = Input.GetAxis("Horizontal") * Time.deltaTime * 150.0f;
+            var z = Input.GetAxis("Vertical") * Time.deltaTime * 5.0f;
+            var y = Input.GetAxis("Pitch") * Time.deltaTime * 150.0f;
+            var r = Input.GetAxis("Roll") * Time.deltaTime * 150.0f;
+
+            transform.Rotate(y, x, r);
+            transform.Translate(0, 0, -z);
+        }
     }
 
 }
