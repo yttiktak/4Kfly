a482a9a0f8c3cb2d0e5399f5c201a5842c542a68
Roberta <repeatingshadow@protonmail.com>
2018-11-26 19:28:02 -0500

    apk version added
    first stab at gyro reading

--- Assets/Scripts/MotionControl.cs
+++ Assets/Scripts/MotionControl.cs
@@ -7,18 +7,22 @@ using UnityEngine.Networking;
 public class MotionControl : NetworkBehaviour {
     Gyroscope gyr;
     void Start() {
-       // if (Input.gyro.enabled) 
+        // if (Input.gyro.enabled) 
         gyr = Input.gyro;
-        Vector3 grav = gyr.gravity;
-        if (grav.magnitude == 0)
-        {
-            gyr.enabled = false;
-        }
-        else
+        //Vector3 grav = gyr.gravity;
+      if (SystemInfo.supportsGyroscope)
         {
             gyr.enabled = true;
         }
+        else
+        {
+            gyr.enabled = false;
+        }
     }
+
+    public Vector2 startPos;
+    public Vector2 direction;
+    public bool directionChosen;
 
     void Update() {
         if (!isLocalPlayer) { return; }
@@ -26,7 +30,31 @@ public class MotionControl : NetworkBehaviour {
         if (gyr.enabled)
         {
             Quaternion gq = gyr.attitude;
-            transform.rotation = gq;
+            Quaternion grq = gq;
+            grq.x = gq.z; // try to flip round y axis
+            grq.z = gq.x;
+            transform.rotation = grq;
+            if (Input.touchCount > 0)
+            {
+                Touch touch = Input.GetTouch(0);
+                switch(touch.phase)
+                {
+                    case TouchPhase.Began:
+                        startPos = touch.position;
+                        directionChosen = false;
+                        break;
+                    case TouchPhase.Moved:
+                        direction = (touch.position - startPos) * 0.01f;
+                        break;
+                    case TouchPhase.Ended:
+                        directionChosen = true;
+                        break;
+                }
+            }
+            if (directionChosen)
+            {
+                transform.Translate(0, 0, - direction.y);
+            }
         }
         else
         {
